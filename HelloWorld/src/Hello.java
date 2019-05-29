b63f1d3da93f194a5faacc35cd6d67533567feec
Venura Komangodage (zpz8zrs) <vkomangodage@ups.com>
2019-05-28 21:35:21 -0400

    Sample code for what are variables.

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,10 +1,16 @@
 public class Hello {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         System.out.println("Hello, Kavi!");
 
-        int myFirstNumber = 5;
-        System.out.println(myFirstNumber);
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myLastOne);
     }
 }
