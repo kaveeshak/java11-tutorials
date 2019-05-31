c05c8e228ecc6e87d252d882dc586ab97598caed
Kavi <kaveeshak@gmail.com>
2019-05-30 22:23:41 -0400

    Added sample code for float and double data types.

--- FloatAndDouble/src/com/java11/tutorials/Main.java
+++ FloatAndDouble/src/com/java11/tutorials/Main.java
@@ -4,15 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //Whole numbers division.
         int myIntValue = 5 / 2;
-        float myFloatValue = 5f / 2f;
-        double myDoubleValue = 5d / 2d;
+        short myShortValue = 20 / 3;
+        System.out.println("myIntValue: " + myIntValue);
+        System.out.println("myShortValue: " + myShortValue);
 
-        //float myFloatValue2 = (float) ((myDoubleValue) / 2);
+        //float data type.
+        float myFloatValue1 = 5f / 2f;
+        float myFloatValue2 = (float) (12.65 / 3f);
+        System.out.println("myFloatValue1: " + myFloatValue1);
+        System.out.println("myFloatValue2: " + myFloatValue2);
 
-        System.out.println("myIntValue: " + myIntValue );
-        System.out.println("myFloatValue: " + myFloatValue);
-        System.out.println("myDoubleValue: " + myDoubleValue);
+        //double data type.
+        double myDoubleValue1 = 5d / 2d;
+        System.out.println("myDoubleValue1: " + myDoubleValue1);
 
         //Quiz
         double pounds = 4d;
