f713ce207e87008c4b8bf60073245cf3ec426a33
Kavi <kaveeshak@gmail.com>
2019-05-30 22:01:22 -0400

    Added char and boolean data type sample code.

---
+++ CharAndBoolean/src/com/java11/tutorials/Main.java
@@ -0,0 +1,20 @@
+package com.java11.tutorials;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //char data type.
+        char myChar1 = 'd';
+        char myChar2 = '#';
+        char myChar3 = '5';
+        char myChar4 = '\u00AE';
+        char myChar5 = '\u3041';
+
+        System.out.println(myChar4);
+        System.out.println(myChar5);
+
+        //boolean data type.
+        boolean isFemale = true;
+    }
+}
