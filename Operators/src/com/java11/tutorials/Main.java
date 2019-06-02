47115c2f02a2bf505e341761727ae86f889e4a8b
Kavi <kaveeshak@gmail.com>
2019-06-01 22:02:54 -0400

    Added sample code for mathematical operators.

---
+++ Operators/src/com/java11/tutorials/Main.java
@@ -0,0 +1,35 @@
+package com.java11.tutorials;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Some operators that performs mathematical operations.
+        int myInt = 30 + 10;
+        System.out.println("30 + 10 = " + myInt);
+
+        myInt = myInt - 2;
+        System.out.println("myInt - 2 = " + myInt);
+
+        myInt = myInt * 4;
+        System.out.println("myInt * 4 = " + myInt);
+
+        myInt = myInt / 2;
+        System.out.println("myInt / 2 = " + myInt);
+
+        myInt = myInt % 3;
+        System.out.println("myInt % 3 = " + myInt);
+
+        myInt++;
+        System.out.println("myInt++ = " + myInt);
+
+        myInt--;
+        System.out.println("myInt * 4 = " + myInt);
+
+        myInt += 14;
+        System.out.println("myInt += 14 = " + myInt);
+
+        myInt %= 6;
+        System.out.println("myInt %= 6 = " + myInt);
+    }
+}
