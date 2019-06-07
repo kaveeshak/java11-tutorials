ee9e31157240905b030e9511b786e7cdf087d134
Kavi <kaveeshak@gmail.com>
2019-06-06 22:47:01 -0400

    Added solutions for exercises.

---
+++ Section4CodingExercises/src/BarkingDog.java
@@ -0,0 +1,16 @@
+public class BarkingDog {
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+
+        boolean isWakeUp = false;
+
+        if((hourOfDay >= 0 && hourOfDay <= 23) &&
+           (hourOfDay > 22 || hourOfDay < 8) &&
+            barking) {
+
+            isWakeUp = true;
+        }
+
+        return isWakeUp;
+    }
+}
