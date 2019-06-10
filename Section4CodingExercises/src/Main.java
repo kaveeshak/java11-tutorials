a4e3350a1bc108b1e622ce2da9f2a771c29958a3
Venura Komangodage (zpz8zrs) <vkomangodage@ups.com>
2019-06-10 16:40:34 -0400

    Added solutions for exercises.

--- Section4CodingExercises/src/Main.java
+++ Section4CodingExercises/src/Main.java
@@ -2,5 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //Testing AreaCalculator
+        System.out.println(AreaCalculator.area(-2));
+        System.out.println(AreaCalculator.area(45.32));
+        System.out.println(AreaCalculator.area(-43, 54.32));
+        System.out.println(AreaCalculator.area(32.12, -54.32));
+        System.out.println(AreaCalculator.area(34.21, 12.43));
     }
 }
