6622cd811fb1279403cf64af8d469dc05c995123
Venura Komangodage (zpz8zrs) <vkomangodage@ups.com>
2019-06-10 18:42:23 -0400

    Added solutions for exercises.

--- Section4CodingExercises/src/Main.java
+++ Section4CodingExercises/src/Main.java
@@ -8,5 +8,11 @@ public class Main {
         System.out.println(AreaCalculator.area(-43, 54.32));
         System.out.println(AreaCalculator.area(32.12, -54.32));
         System.out.println(AreaCalculator.area(34.21, 12.43));
+
+        //Testing MinutesToYearsDaysCalculator
+        MinutesToYearsDaysCalculator.printYearsAndDays(-34);
+        MinutesToYearsDaysCalculator.printYearsAndDays(525600);
+        MinutesToYearsDaysCalculator.printYearsAndDays(1051200);
+        MinutesToYearsDaysCalculator.printYearsAndDays(561600);
     }
 }
