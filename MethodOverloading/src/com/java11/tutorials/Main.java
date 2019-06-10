63029a83b88c08e1a71ca04c0ac1365ca3c41a19
Kavi <kaveeshak@gmail.com>
2019-06-09 22:57:15 -0400

    Added sample code for method overloading.

--- MethodOverloading/src/com/java11/tutorials/Main.java
+++ MethodOverloading/src/com/java11/tutorials/Main.java
@@ -2,6 +2,8 @@ package com.java11.tutorials;
 
 public class Main {
 
+    public static final String INVALID_ERROR_MESSAGE = "Invalid value";
+
     public static void main(String[] args) {
 
         //Method Overloading.
@@ -17,7 +19,57 @@ public class Main {
         System.out.println(calcFeetAndInchesToCentimeters(24));
         System.out.println(calcFeetAndInchesToCentimeters(0));
 
+        //Quiz
+        System.out.println(getDurationString(-34, 14));
+        System.out.println(getDurationString(4, 87));
+        System.out.println(getDurationString(128, 43));
 
+        System.out.println(getDurationString(-54));
+        System.out.println(getDurationString(134));
+        System.out.println(getDurationString(1200));
+        System.out.println(getDurationString(39600));
+
+
+
+    }
+
+    public static String getDurationString(int seconds) {
+
+        if(seconds < 0) {
+            return INVALID_ERROR_MESSAGE;
+        }
+
+        int minutes = seconds / 60;
+        int remainingSeconds = seconds % 60;
+
+        return getDurationString(minutes, remainingSeconds);
+    }
+
+    public static String getDurationString(int minutes, int seconds) {
+
+        if(minutes < 0 || (seconds < 0 || seconds > 59)) {
+            return INVALID_ERROR_MESSAGE;
+        }
+
+        int hours = minutes / 60;
+        int remainingMinutes = minutes % 60;
+
+        String hoursString = hours + "h ";
+        if(hours < 10) {
+            hoursString = "0" + hoursString;
+        }
+
+        String minutesString = remainingMinutes + "m ";
+        if(remainingMinutes < 10) {
+            minutesString = "0" + minutesString;
+        }
+
+        String secondsString = seconds + "s";
+        if(seconds < 10) {
+            secondsString = "0" + secondsString;
+        }
+
+        return hoursString + minutesString + secondsString;
     }
 
     public static double calcFeetAndInchesToCentimeters(int inches) {
