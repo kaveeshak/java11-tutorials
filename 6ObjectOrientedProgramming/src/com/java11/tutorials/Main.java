d5a443805c90967d77b2efeab4eaf10e6045c246
kaveesha <kaveeshak@gmail.com>
2019-09-01 10:11:35 -0400

    SimpleCalculator coding exercise.

--- 6ObjectOrientedProgramming/src/com/java11/tutorials/Main.java
+++ 6ObjectOrientedProgramming/src/com/java11/tutorials/Main.java
@@ -1,5 +1,7 @@
 package com.java11.tutorials;
 
+import com.java11.exercises.SimpleCalculator;
+
 public class Main {
 
     public static void main(String [] args) {
@@ -10,5 +12,15 @@ public class Main {
         System.out.println(toyotaCar.getModel());
         toyotaCar.setModel("4Runner");
         System.out.println(toyotaCar.getModel());
+
+        //Testing SimpleCalculator
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+        simpleCalculator.setFirstNumber(12);
+        simpleCalculator.setSecondNumber(3);
+        System.out.println(simpleCalculator.getAdditionResult());
+        System.out.println(simpleCalculator.getMultiplicationResult());
+        System.out.println(simpleCalculator.getDivisionResult());
+        simpleCalculator.setSecondNumber(0);
+        System.out.println(simpleCalculator.getDivisionResult());
     }
 }
