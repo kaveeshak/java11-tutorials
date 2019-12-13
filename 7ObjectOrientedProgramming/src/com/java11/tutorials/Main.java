860e407fb0c3e8df95e99a6a466345eb2319499a
Venura Komangodage (zpz8zrs) <vkomangodage@ups.com>
2019-12-13 09:06:36 -0500

    Challenge on Composition.

--- 7ObjectOrientedProgramming/src/com/java11/tutorials/Main.java
+++ 7ObjectOrientedProgramming/src/com/java11/tutorials/Main.java
@@ -1,5 +1,7 @@
 package com.java11.tutorials;
 
+import com.java11.exercises.*;
+
 public class Main {
     public static void main(String [] args) {
         //Demo on Composition.
@@ -12,5 +14,22 @@ public class Main {
         pc.getMonitor().drawPixelAt(1500, 1200, "red");
         pc.getMotherboard().loadProgram("Windows 1.0");
         pc.getTheCase().pressPowerButton();
+
+        //Testing Composition challenge.
+        Wall wall1 = new Wall("West");
+        Wall wall2 = new Wall("South");
+        Wall wall3 = new Wall("East");
+        Wall wall4 = new Wall("North");
+
+        Ceiling ceiling = new Ceiling(12, 55);
+
+        Bed bed = new Bed("Modern", 4, 3, 2, 1);
+
+        Lamp lamp = new Lamp("Classic", false, 75);
+
+        Bedroom bedroom = new Bedroom("Tims", wall1, wall2, wall3, wall4, ceiling, bed, lamp);
+
+        bedroom.makeBed();
+        bedroom.getLamp().turnOn();
     }
 }
