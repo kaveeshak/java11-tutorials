d3069f16134ed52abf2ef961cf36d07e27409a6b
Venura Komangodage (zpz8zrs) <vkomangodage@ups.com>
2020-02-18 15:11:50 -0500

    Added samples of primitive data types. byte, short, int and long. Reviewed. v1.

--- 3FirstSteps/src/com/java11/tutorials/Main.java
+++ 3FirstSteps/src/com/java11/tutorials/Main.java
@@ -1,24 +1,36 @@
 package com.java11.tutorials;
 
 public class Main {
-
     public static void main(String[] args) {
+        //Variables.
+        System.out.println("Variables...");
+        int age = 26;
+        int newMiles = 3421 + 52;
+        System.out.println(age);
+        System.out.println("newMiles");
 
+        //Primitive data types.
+        System.out.println("Primitive data types...");
+
+        //int data type.
         //From -2.147 to +2.147 billions.
         int myIntNumber = 21_000_000;
         int myNewIntNumber = (myIntNumber / 2);
         System.out.println("myNewIntNumber: " + myNewIntNumber);
 
+        //byte data type.
         //From -128 to +127
         byte myByteNumber = 100;
         byte myNewByteNumber = (byte) (myByteNumber / 2);
         System.out.println("myNewByteNumber: " + myNewByteNumber);
 
+        //short data type.
         //From -32768 to +32767
         short myShortNumber = 29_028;
         short myNewShortNumber = (short) (myShortNumber / 2);
         System.out.println("myNewShortNumber: " + myNewShortNumber);
 
+        //long data type.
         // From -(2^63) to +((2^63) - 1)
         long myLongNumber = 9223372036854775807L;
 
