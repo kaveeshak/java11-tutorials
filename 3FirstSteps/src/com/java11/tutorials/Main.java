ed8e54f5b4390cb53dc49adf85ae074d44f2a57e
venurakomangodage <kaveeshak@gmail.com>
2020-02-19 11:02:57 -0500

    Reviewed whole numbers division and data types for storing floating point numbers.

--- 3FirstSteps/src/com/java11/tutorials/Main.java
+++ 3FirstSteps/src/com/java11/tutorials/Main.java
@@ -40,5 +40,27 @@ public class Main {
         int number3 = 50;
         long number4 = 50000L + (10 * (number1 + number2 + number3));
         System.out.println("number4 variable value:" + number4);
+
+        //Whole numbers division.
+        System.out.println("Division with whole numbers...");
+        int myIntValue = 5 / 2;
+        short myShortValue = 20 / 3;
+        System.out.println("myIntValue: " + myIntValue);
+        System.out.println("myShortValue: " + myShortValue);
+
+        //float data type.
+        float myFloatValue1 = 5f / 2f;
+        float myFloatValue2 = (float) (12.65 / 3f);
+        System.out.println("myFloatValue1: " + myFloatValue1);
+        System.out.println("myFloatValue2: " + myFloatValue2);
+
+        //double data type.
+        double myDoubleValue1 = 5d / 2d;
+        System.out.println("myDoubleValue1: " + myDoubleValue1);
+
+        //Quiz
+        double pounds = 4d;
+        double kilos = (pounds * 0.45359237d);
+        System.out.println(pounds + " pounds = " + kilos + " kilos.");
     }
 }
