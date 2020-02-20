fdb2ed006da5220817394216a0875e9b324ab9df
venurakomangodage <kaveeshak@gmail.com>
2020-02-19 19:51:31 -0500

    Reviewed String data type.

--- 3FirstSteps/src/com/java11/tutorials/Main.java
+++ 3FirstSteps/src/com/java11/tutorials/Main.java
@@ -77,5 +77,21 @@ public class Main {
         //boolean data type.
         boolean isFemale = true;
         System.out.println(isFemale);
+
+        //String data type.
+        System.out.println();
+        System.out.println("String data type...");
+        String myString = "This is my first string.";
+        System.out.println(myString);
+        myString = myString + " We can add more.";
+        System.out.println(myString);
+        System.out.println(myString + " 2015 \u00A9");
+        System.out.println(myString);
+
+        String myLastString = "My number is 34.26";
+        int myInt = 5;
+        boolean myBoolean = true;
+        myLastString = myLastString + myInt + myBoolean;
+        System.out.println(myLastString);
     }
 }
