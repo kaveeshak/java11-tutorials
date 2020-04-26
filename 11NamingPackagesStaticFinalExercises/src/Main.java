e68e1a10ef66ea8f4a94b9b597084a05ef8d71e8
venurakomangodage <kaveeshak@gmail.com>
2020-04-26 11:18:30 -0400

    Solution for creating a package, then a library and importing the jar file to a new project.

--- 11NamingPackagesStaticFinalExercises/src/Main.java
+++ 11NamingPackagesStaticFinalExercises/src/Main.java
@@ -1,19 +1,21 @@
+import com.java11.exercises.Series;
+
 public class Main {
     public static void main(String[] args) {
         //Testing exercise on packages...
         System.out.println("Testing exercise on packages...");
         for (int i = 0; i <= 10; i++) {
-            System.out.println(Series.);
+            System.out.print(Series.nSum(i) + " ");
         }
         System.out.println();
 
         for (int i = 0; i <= 10; i++) {
-
+            System.out.print(Series.factorial(i) + " ");
         }
         System.out.println();
 
         for (int i = 0; i <= 10; i++) {
-
+            System.out.print(Series.fibonacci(i) + " ");
         }
     }
 }
